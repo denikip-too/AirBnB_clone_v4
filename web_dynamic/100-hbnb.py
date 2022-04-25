11fbad2d53bb6140a0aebacfea40e379e6c47e5d
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-04-25 08:50:19 -0700

    update route

--- web_dynamic/100-hbnb.py
+++ web_dynamic/100-hbnb.py
@@ -25,7 +25,6 @@ def hbnb():
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     st_ct = []
-    cache_id = str(uuid.uuid4())
 
     for state in states:
         st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
@@ -40,7 +39,7 @@ def hbnb():
                            states=st_ct,
                            amenities=amenities,
                            places=places,
-                           cache_id=cache_id)
+                           cache_id=uuid.uuid4())
 
 
 if __name__ == "__main__":
