c04c5851a1cb6ce70f6f26c0f1e157385ac0955d
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-04-24 23:34:32 -0700

    JS script

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,11 @@
+// JavaScript script
+
+$('input[name=checkbox]').change(function() {
+  if ($(this).is(':checked')) {
+    amenityObj[$(this).attr('data-name')] = $(this).attr('data-id');
+  } else {
+    delete amenityObj[$(this).attr('data-name')];
+  }
+  const names = Object.keys(amenityObj);
+  $('.amenities h4').text(names.sort().join(', '));
+});
